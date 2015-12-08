a7975b5091e820847950b285263bec68b0ed43ea
GaryBabich <gababich2@gmail.com>
2015-12-07 22:15:56 -0800

    Create WealthManager.java

---
+++ WealthManager.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+public class WealthManager {
+
+    public static void main(String[] args){ 
+      	WealthManagerForm p = new WealthManagerForm();
+	p.Setup();
+	p.setSize(600,600);
+	p.setVisible(true);
+	
+        
+    }    
+}
